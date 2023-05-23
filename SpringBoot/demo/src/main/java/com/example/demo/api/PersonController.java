fb8f11c5240916084942afb53d1b72ad1d13d473
Dan <79495801+bljptrpl@users.noreply.github.com>
2023-05-23 11:29:32 -0600

    setting up initial classes for database layer

---
+++ SpringBoot/demo/src/main/java/com/example/demo/api/PersonController.java
@@ -0,0 +1,14 @@
+package com.example.demo.api;
+import com.example.demo.service.PersonService;
+import com.example.demo.model.Person;
+
+public class PersonController {
+    private final PersonService personService;
+
+    public PersonController(PersonService personService){
+        this.personService = personService;
+    }
+    public void addPerson(Person person) {
+        personService.addPerson(person);
+    }
+}
---
+++ SpringBoot/demo/src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+import java.util.UUID;
+public class Person {
+
+    private final UUID id;
+    private final String name;
+
+    public Person(UUID id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public UUID getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
