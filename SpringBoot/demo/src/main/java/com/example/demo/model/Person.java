4c325ac658e8d8bdd3f59dff078b8acef8f5a9ec
Dan <79495801+bljptrpl@users.noreply.github.com>
2023-06-03 01:26:19 -0600

    add dependencies to pom for @NotBlank to work

--- SpringBoot/demo/src/main/java/com/example/demo/model/Person.java
+++ SpringBoot/demo/src/main/java/com/example/demo/model/Person.java
@@ -1,13 +1,15 @@
 package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.UUID;
 public class Person {
 
     private final UUID id;
 
     private final String name;
-
+    @NotBlank // 1:00:05
     public Person(@JsonProperty("id") UUID id,
                   @JsonProperty("name") String name) {
         this.name = name;
